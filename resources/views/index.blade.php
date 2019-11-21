ec3ec0eab241f5ec5809b41b24c5f44c922bd697
Frank Ponte <frankponte95@gmail.com>
2019-11-21 17:33:01 -0400

    Header (STILL NOT 100% DONE)

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')      
+
+@section('title','Index')
+
+@section('content')
+<section>
+    <div style="margin-top:70px">
+        index page
+    </div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="favicon.ico">
+
+    <title>@yield('title') - tuPropiedad</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+
+<header>
+    <nav class="navbar navbar-expand-md sticky-top">
+        <!-- One of the primary actions on mobile is to call a business - This displays a phone button on mobile only -->
+        <div class="navbar-toggler-right">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+        </div>
+
+        <div class="collapse navbar-collapse flex-column" id="navbar">
+            <!-- First navbar ROW -->
+            <ul class="navbar-nav bg-white ml-auto h-100">
+                <li class="nav-item bg-yellow ry-corners mr-2">
+                        <a class="nav-link mx-3" href="#">Iniciar sesión</a>
+                </li>
+                <li class="nav-item bg-yellow ry-corners">
+                        <a class="nav-link mx-3" href="#">Registrarse</a>
+                </li>
+            </ul>
+            <!-- Second navbar ROW -->
+            <ul class="navbar-nav justify-content-center w-100 bg-blue">
+                <li class="nav-item active mr-7">
+                    <a class="nav-link" href="#">Inicio</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Vivienda</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Servicios</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Empleo</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Ayuda</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Contáctenos</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Conócemos más</a>
+                </li>
+                <li class="nav-item mr-7">
+                    <a class="nav-link" href="#">Idioma</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<!-- Begin page content -->
+<main role="main" class="container">
+            @yield('content')
+</main>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
