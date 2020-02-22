fea9dea5fc9b5f1af9b75bbba24dc3689bdec4ee
Jose Tirado <jose_enrique_29@hotmail.com>
2020-02-22 12:51:12 -0400

    particular search advance

--- public/js/app.js
+++ public/js/app.js
@@ -66,18 +66,25 @@ $('.btn-number').click(function(e){
            if(parseInt(input.val()) == input.attr('max')) {
                $(this).attr('disabled', true);
            }
-
        }
    } else {
        input.val(0);
    }
 });
 
-$('.input-number').focusin(function(){
+$('#counterBath').focusin(function(){
   $(this).data('oldValue', $(this).val());
 });
 
-$('.input-number').change(function() {
+$('#counterRoom').focusin(function(){
+   $(this).data('oldValue', $(this).val());
+ });
+
+$('#counterParking').focusin(function(){
+   $(this).data('oldValue', $(this).val());
+});
+
+$('#counterBath').change(function() {
    
    minValue =  parseInt($(this).attr('min'));
    maxValue =  parseInt($(this).attr('max'));
@@ -98,7 +105,85 @@ $('.input-number').change(function() {
    }
 });
 
-$(".input-number").keydown(function (e) {
+
+$('#counterRoom').change(function() {
+   
+   minValue =  parseInt($(this).attr('min'));
+   maxValue =  parseInt($(this).attr('max'));
+   valueCurrent = parseInt($(this).val());
+   
+   name = $(this).attr('name');
+   if(valueCurrent >= minValue) {
+       $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+   } else {
+       alert('Sorry, the minimum value was reached');
+       $(this).val($(this).data('oldValue'));
+   }
+   if(valueCurrent <= maxValue) {
+       $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+   } else {
+       alert('Sorry, the maximum value was reached');
+       $(this).val($(this).data('oldValue'));
+   }
+});
+
+
+
+$('#counterPark').change(function() {
+   
+   minValue =  parseInt($(this).attr('min'));
+   maxValue =  parseInt($(this).attr('max'));
+   valueCurrent = parseInt($(this).val());
+   
+   name = $(this).attr('name');
+   if(valueCurrent >= minValue) {
+       $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+   } else {
+       alert('Sorry, the minimum value was reached');
+       $(this).val($(this).data('oldValue'));
+   }
+   if(valueCurrent <= maxValue) {
+       $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+   } else {
+       alert('Sorry, the maximum value was reached');
+       $(this).val($(this).data('oldValue'));
+   }
+});
+
+
+$("#counterBath").keydown(function (e) {
+   // Allow: backspace, delete, tab, escape, enter and .
+   if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+      // Allow: Ctrl+A
+      (e.keyCode == 65 && e.ctrlKey === true) || 
+      // Allow: home, end, left, right
+      (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+   }
+   // Ensure that it is a number and stop the keypress
+   if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+   }
+});
+
+$("#counterRoom").keydown(function (e) {
+   // Allow: backspace, delete, tab, escape, enter and .
+   if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+      // Allow: Ctrl+A
+      (e.keyCode == 65 && e.ctrlKey === true) || 
+      // Allow: home, end, left, right
+      (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+   }
+   // Ensure that it is a number and stop the keypress
+   if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+   }
+});
+
+$("#counterPark").keydown(function (e) {
       // Allow: backspace, delete, tab, escape, enter and .
       if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
          // Allow: Ctrl+A
