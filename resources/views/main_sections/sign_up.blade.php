84d0fa3aa62c3cb0fe125c5633c1d2f41e6ac5b6
fuhranku <frankponte95@gmail.com>
2020-02-22 00:10:44 -0400

    UPDATE::Checkbox change

--- resources/views/main_sections/sign_up.blade.php
+++ resources/views/main_sections/sign_up.blade.php
@@ -377,12 +377,12 @@
         <div class="row mt-5">
             <div class="col-md-4 mr-5"></div>
             <div class="col-md-1 form-check ">
-                <input type="checkbox" class="form-check-input" id="portal-web">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
                 <label class="form-check-label" for="portal-web">Español</label>
             </div>
             <div class="col-md-1"></div>
             <div class="col-md-1 form-check">
-                <input type="checkbox" class="form-check-input" id="portal-web">
+                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3">
                 <label class="form-check-label" for="portal-web">Inglés</label>
             </div>
         </div>
