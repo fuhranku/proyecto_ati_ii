3851a9da6944bdba69573d8b8fa1cba62718877d
yuli-ferna <yuliferna123@gmail.com>
2020-02-22 22:01:58 -0400

    forgot windows (page 2) with margin, ready

--- resources/views/main_sections/sign_in.blade.php
+++ resources/views/main_sections/sign_in.blade.php
@@ -94,20 +94,26 @@
                     <form>
                         <div class="form-group">
                         <div class="row">
+                            <div class="col">
                                 <input type="radio" id="id" name="forgot" value="id">
                                 <label for="id"> Cédula de identidad, DNI o pasaporte</label><br>
+                            </div>
                         </div>
                         </div>
                         <div class="form-group">
                         <div class="row">
+                            <div class="col">
                                 <input type="radio" id="email" name="forgot" value="email">
                                 <label for="email"> Correo electrónico o usuario</label><br>
+                            </div>
                         </div>
                         </div>
                         <div class="form-group">
                         <div class="row">
+                            <div class="col">
                                 <input type="radio" id="phone" name="forgot" value="phone">
                                 <label for="phone"> Teléfono celular o móvil</label><br>
+                            </div>
                         </div>
                         </div>
                         <div class="button-group">
@@ -130,13 +136,13 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title">Recuperar contraseña, o mis datos</h4>
-                        <button type="button" onclick="closeModal('forgot')" class="close" data-dismiss="modal">
+                        <button type="button" onclick="closeModal('forgot_window');resetForgot()" class="close" data-dismiss="modal">
                             <span>-</span>
                         </button>
                         <button type="button" class="icon" data-dismiss="modal">
                             <span>&#9633;</span>
                         </button>
-                        <button type="button"  onclick="closeModal('forgot')" class="close" data-dismiss="modal">
+                        <button type="button"  onclick="closeModal('forgot_window');resetForgot()" class="close" data-dismiss="modal">
                             <span>×</span>
                         </button>
                     </div>
@@ -156,20 +162,26 @@
                             <form>
                                 <div class="form-group">
                                     <div class="row">
-                                        
+                                        <div class="col">
                                             <label for="Email">Ingresa tu correo electrónico, o usuario</label>
-                                        
                                         </div>
+                                        
+                                    </div>
                                     <div class="row">
-                                        <input type="text" class="form-control" name="Email"/>
-                                        {!! $errors->first('Email', '<span class="help-block">:message</span>') !!}
+                                        <div class="col">
+                                            <input type="text" class="form-control" name="Email"/>
+                                            {!! $errors->first('Email', '<span class="help-block">:message</span>') !!}
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="button-group">
                                     <br>
-                                    <button type="submit" class="btn modal-button">Aceptar</button>
+                                    <button type="submit" onclick="closeModal('forgot_window');resetForgot()" class="btn modal-button">Aceptar</button>
+                                    <button type="button" onclick="closeModal('forgot_window');resetForgot()" class="btn modal-button">Cancelar</button>
                                     <br>
+    
                                 </div>
+                            </form>
                             </form>
                         </div>
                         <!-- end recuperar email -->
@@ -189,22 +201,27 @@
                             <form>
                                 <div class="form-group">
                                 <div class="row">
-                                    
-                                    <label for="Id">Ingresa tu cédula de identidad, DNI, o pasaporte</label>
+                                    <div class="col">
+                                        <label for="Id">Ingresa tu cédula de identidad, DNI, o pasaporte</label>
+                                    </div>
                                     
                                 </div>
                                 <div class="row">
+                                    <div class="col">
                                         <input type="text" class="form-control" name="Id"/>
                                         {!! $errors->first('Id', '<span class="help-block">:message</span>') !!}
                                     </div>
                                 </div>
+                                </div>
                                 
                                 <div class="button-group">
                                     <br>
-                                    <button type="submit" class="btn modal-button">Aceptar</button>
+                                    <button type="submit" onclick="closeModal('forgot_window');resetForgot()" class="btn modal-button">Aceptar</button>
+                                    <button type="button" onclick="closeModal('forgot_window');resetForgot()" class="btn modal-button">Cancelar</button>
                                     <br>
     
                                 </div>
+                            </form>
                             </form>
                             
                         </div>
@@ -224,20 +241,22 @@
                             <form>
                                 <div class="form-group">
                                 <div class="row">
-                                    
-                                    <label for="Phone">Ingresa tu número de teléfono móvil</label>
-                                    
+                                    <div class="col">
+                                        <label for="Phone">Ingresa tu número de teléfono móvil</label>
+                                    </div>
                                 </div>
                                 <div class="row">
-                                    <input type="text" class="form-control" name="Phone"/>
-                                    {!! $errors->first('Phone', '<span class="help-block">:message</span>') !!}
+                                    <div class="col">
+                                        <input type="text" class="form-control" name="Phone"/>
+                                        {!! $errors->first('Phone', '<span class="help-block">:message</span>') !!}
+                                    </div>
                                 </div>
-                                
                                 </div>
                                 
                                 <div class="button-group">
                                     <br>
-                                    <button type="submit" class="btn modal-button">Aceptar</button>
+                                    <button type="submit" onclick="closeModal('forgot_window');resetForgot()" class="btn modal-button">Aceptar</button>
+                                    <button type="button" onclick="closeModal('forgot_window');resetForgot()" class="btn modal-button">Cancelar</button>
                                     <br>
     
                                 </div>
