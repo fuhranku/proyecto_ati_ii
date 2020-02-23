c6ac4c5ab764bb07271881761b8f3a4f32cf42b0
yuli-ferna <yuliferna123@gmail.com>
2020-02-22 21:18:00 -0400

    forgot windows (page 2), ready

--- public/js/sign_in.js
+++ public/js/sign_in.js
@@ -1,20 +1,48 @@
-function initPage(){
+function initPage()
+{
     document.getElementById("sign_in").style.display="contents";
     document.getElementById("sign_in").style.overflow="auto";
+    resetForgot();
+}
+function resetForgot() {
+    document.getElementById("forgot_email").style.display="none";
+    document.getElementById("forgot_id").style.display="none";
+    document.getElementById("forgot_phone").style.display="none";
     
 }
 
-function closeModal()
+function openModal(idModal)
+{
+    document.getElementById(idModal).style.display="contents";
+    document.getElementById(idModal).style.overflow="auto";
+
+}
+
+function closeModal(idModal)
 {    
-    document.getElementById("sign_in").style.display="none";
+    document.getElementById(idModal).style.display="none";
 }
 
-function maxModal(){
-
-}
-
-function minModal(){
+function maxModal()
+{
 
 }
 
+function minModal()
+{
+
+}
+
+function optionRadio(name){
+    var radios = document.getElementsByName(name);
+    for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+    
+        let varName= 'forgot_' + radios[i].value;
+        openModal(varName);
+
+        break;
+    }
+    }
+}
 initPage();
--- resources/views/main_sections/sign_in.blade.php
+++ resources/views/main_sections/sign_in.blade.php
@@ -13,13 +13,13 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title">Iniciar sesión</h4>
-                        <button type="button" onclick="closeModal()" class="close" data-dismiss="modal">
+                        <button type="button" onclick="closeModal('sign_in')" class="close" data-dismiss="modal">
                             <span>-</span>
                         </button>
                         <button type="button" class="icon" data-dismiss="modal">
                             <span>&#9633;</span>
                         </button>
-                        <button type="button"  onclick="closeModal()" class="close" data-dismiss="modal">
+                        <button type="button"  onclick="closeModal('sign_in')" class="close" data-dismiss="modal">
                             <span>×</span>
                         </button>
                     </div>
@@ -62,7 +62,7 @@
                             <br>
                             <button type="submit" class="btn modal-button">Send</button>
                             <br>
-                            <a class="modal-forgot" href="">Olvidé mi contraseña, o mis datos</a>
+                            <a id="modal-forgot" data-toggle="modal" data-target="#forgot">Olvidé mi contraseña, o mis datos</a>
                             <!-- <a href="{{ route('main.sign_in') }}" class="btn btn-info btn-block" >{{__('words.back')}}</a> -->
 
                         </div>
@@ -74,10 +74,184 @@
             </div>
         </div>
         <!-- forgot -->
-
-        <!-- recuperar email -->
-
-        <!-- recuperar con cédula -->
+        <div class="modal fade" id="forgot">
+            <div class="modal-dialog ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Recuperar contraseña, o mis datos</h4>
+                        <button type="button" onclick="closeModal('forgot')" class="close" data-dismiss="modal">
+                            <span>-</span>
+                        </button>
+                        <button type="button" class="icon" data-dismiss="modal">
+                            <span>&#9633;</span>
+                        </button>
+                        <button type="button"  onclick="closeModal('forgot')" class="close" data-dismiss="modal">
+                            <span>×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                    <p>Seleccione la información que va a proporcionar</p>
+                    <form>
+                        <div class="form-group">
+                        <div class="row">
+                                <input type="radio" id="id" name="forgot" value="id">
+                                <label for="id"> Cédula de identidad, DNI o pasaporte</label><br>
+                        </div>
+                        </div>
+                        <div class="form-group">
+                        <div class="row">
+                                <input type="radio" id="email" name="forgot" value="email">
+                                <label for="email"> Correo electrónico o usuario</label><br>
+                        </div>
+                        </div>
+                        <div class="form-group">
+                        <div class="row">
+                                <input type="radio" id="phone" name="forgot" value="phone">
+                                <label for="phone"> Teléfono celular o móvil</label><br>
+                        </div>
+                        </div>
+                        <div class="button-group">
+                            <br>
+                            <button type="button" onclick="optionRadio('forgot')" data-toggle="modal" data-target="#forgot_window"  data-dismiss="modal" class="btn modal-button" >Aceptar</button>
+                            <button type="button" class="btn modal-button">Cancelar</button>
+                            <br>
+                            <!-- <a href="{{ route('main.sign_in') }}" class="btn btn-info btn-block" >{{__('words.back')}}</a> -->
+                        </div>
+                    </form>
+                    </div>
+                    <div class="modal-footer">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end forgot -->
+        <div class="modal fade" id="forgot_window">
+            <div class="modal-dialog ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Recuperar contraseña, o mis datos</h4>
+                        <button type="button" onclick="closeModal('forgot')" class="close" data-dismiss="modal">
+                            <span>-</span>
+                        </button>
+                        <button type="button" class="icon" data-dismiss="modal">
+                            <span>&#9633;</span>
+                        </button>
+                        <button type="button"  onclick="closeModal('forgot')" class="close" data-dismiss="modal">
+                            <span>×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- recuperar email -->
+                        <div id="forgot_email">
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <br>
+                            @endif
+                            <form>
+                                <div class="form-group">
+                                    <div class="row">
+                                        
+                                            <label for="Email">Ingresa tu correo electrónico, o usuario</label>
+                                        
+                                        </div>
+                                    <div class="row">
+                                        <input type="text" class="form-control" name="Email"/>
+                                        {!! $errors->first('Email', '<span class="help-block">:message</span>') !!}
+                                    </div>
+                                </div>
+                                <div class="button-group">
+                                    <br>
+                                    <button type="submit" class="btn modal-button">Aceptar</button>
+                                    <br>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- end recuperar email -->
+                        <!-- recuperar con cédula -->
+                        <div id="forgot_id">
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            <br>
+                            @endif
+                        
+                            <form>
+                                <div class="form-group">
+                                <div class="row">
+                                    
+                                    <label for="Id">Ingresa tu cédula de identidad, DNI, o pasaporte</label>
+                                    
+                                </div>
+                                <div class="row">
+                                        <input type="text" class="form-control" name="Id"/>
+                                        {!! $errors->first('Id', '<span class="help-block">:message</span>') !!}
+                                    </div>
+                                </div>
+                                
+                                <div class="button-group">
+                                    <br>
+                                    <button type="submit" class="btn modal-button">Aceptar</button>
+                                    <br>
+    
+                                </div>
+                            </form>
+                            
+                        </div>
+                        <!-- end recuperar con cédula -->
+                        <!-- recuperar con cédula -->
+                        <div id="forgot_phone">
+                            @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div><br>
+                            @endif
+                        
+                            <form>
+                                <div class="form-group">
+                                <div class="row">
+                                    
+                                    <label for="Phone">Ingresa tu número de teléfono móvil</label>
+                                    
+                                </div>
+                                <div class="row">
+                                    <input type="text" class="form-control" name="Phone"/>
+                                    {!! $errors->first('Phone', '<span class="help-block">:message</span>') !!}
+                                </div>
+                                
+                                </div>
+                                
+                                <div class="button-group">
+                                    <br>
+                                    <button type="submit" class="btn modal-button">Aceptar</button>
+                                    <br>
+    
+                                </div>
+                            </form>
+                            </div>
+                            
+                        </div>
+                        <!-- end recuperar con cédula -->
+                    </div>
+                    <!-- <div class="modal-footer">
+                    </div> -->
+                </div>
+            </div>
+        </div>
     </div>
     <script src="{{ asset('js/sign_in.js') }}"></script>
 
