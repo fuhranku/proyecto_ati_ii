ac5c33eb1173792b51edee3a09b70f6d91be70e3
fuhranku <frankponte95@gmail.com>
2020-02-25 01:03:39 -0400

    UPDATE::minor sign_up changes

--- resources/views/main_sections/sign_up.blade.php
+++ resources/views/main_sections/sign_up.blade.php
@@ -48,33 +48,7 @@
                 </div>
             </div>
     </section>
-    {{-- Modal --}}
-        <div class="row h-100 d-none" id="sign-up-modal">
-            <div class="col-sm-12 my-auto">
-            <div class="w-30 mx-auto text-center bg-white pb-3">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="bg-blue text-white p-2">
-                            Registro Exitoso
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-12 mt-2">
-                        ¿Qué desea hacer a continuación?
-                    </div>
-                </div>
-                <div class="row mt-5">
-                    <div class="col-sm-6 mt-2">
-                        <a class="btn btn-yellow text-center" href="#">Iniciar sesión</a>
-                    </div>
-                    <div class="col-sm-6 mt-2">
-                        <a class="btn btn-yellow text-center" href="{{ url('index') }}">Volver al inicio</a>
-                    </div>
-                </div>
-                </div>
-            </div>
-        </div>
+    @include('sign_up_section.sign_up_modal')
 </form>
 
 {{-- <script type="text/javascript">
