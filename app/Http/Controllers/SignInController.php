b0ab0ad43f6d55fa527cb8272d1416f98154d238
yuli-ferna <yuliferna123@gmail.com>
2020-03-14 13:08:42 -0400

    Hash arreglado

--- app/Http/Controllers/SignInController.php
+++ app/Http/Controllers/SignInController.php
@@ -23,37 +23,47 @@ class SignInController extends Controller
             'email' => 'email|required|string',
             'password' => 'required|string'
         ]);
-        // dd($credentials);
+        // echo Hash::make('testmegatest123');
+
         $emailQ = $request->get('email');
-        $passQ = Hash::make($request->get('password'));
+        $passQ = $request->get('password');
         // $emailQ = 'testmegatest@gmail.com';
         // $passQ = '$2y$10$CRWVCdC09mlI7dGK9kq8a.zrGDxU03eWKnYQZQ6KO8dO6Dup2ZWEC';
         
-        $emailQ = 'asd@sd.vs';
-        $passQ = '$2y$10$0BL4UZmQ0X9tXu2PgV.UtebS0Bp7/3QKPYwHD2Yk8BRtGeDKPVdL.';
-        $queryUser = DB::table('users')->select('*')->where('email', '=', $emailQ)->where('password', '=', $passQ)->first();
+        // $emailQ = 'asd@sd.vs';
+        // $passQ = '$2y$10$0BL4UZmQ0X9tXu2PgV.UtebS0Bp7/3QKPYwHD2Yk8BRtGeDKPVdL.';
+        $queryUser = DB::table('users')->select('*')->where('email', '=', $emailQ)->first();
         
-        if ($queryUser) {
-            if ($queryUser->person_type == 'nat') {
-                $queryUserSpe = DB::table('naturalPeople')->select('*')->where('user_id', '=', $queryUser->id)->first();
-            } else {
-                $queryUserSpe = DB::table('legalPeople')->select('*')->where('user_id', '=', $queryUser->id)->first();
+        if ($queryUser) 
+        {
+            //comprobar password
+            if (Hash::check($passQ, $queryUser->password)) {
+                if ($queryUser->person_type == 'nat') {
+                    $queryUserSpe = DB::table('naturalPeople')->select('*')->where('user_id', '=', $queryUser->id)->first();
+                } else {
+                    $queryUserSpe = DB::table('legalPeople')->select('*')->where('user_id', '=', $queryUser->id)->first();
+                }
+                // echo $queryUser->email;
+                // dd($queryUser);
+                // print_r($queryUser);
+                // die();
+                // $request->session()->put('info', $queryUser);
+                // session(['info' => $queryUser]);
+                Session::put('info', $queryUser);
+                Session::put('info_specific', $queryUserSpe);
+                $sessionInfo = Session::get('info');
+                // echo $sessionInfo->email;
+                // dd($sessionInfo);
+                // $reqInfo = $request->session()->get('info');
+                // $result = json_decode($reqInfo, true);
+                // dd( $reqInfo->email);
+    
+                return redirect()->back();
+                
+            }else {
+                return redirect()->back()
+                        ->withErrors(['email' => 'Invalid input']);    
             }
-            // echo $queryUser->email;
-            // dd($queryUser);
-            // print_r($queryUser);
-            // die();
-            // $request->session()->put('info', $queryUser);
-            // session(['info' => $queryUser]);
-            Session::put('info', $queryUser);
-            Session::put('info_specific', $queryUserSpe);
-            $sessionInfo = Session::get('info');
-            // echo $sessionInfo->email;
-            // dd($sessionInfo);
-            return redirect()->back();
-            // $reqInfo = $request->session()->get('info');
-            // $result = json_decode($reqInfo, true);
-            // dd( $reqInfo->email);
         }
         else {
             return redirect()->back()
