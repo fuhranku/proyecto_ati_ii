a9ee63e291042b8043dcf288b5318b6c8927035b
Jose Tirado <jose_enrique_29@hotmail.com>
2020-03-14 14:01:50 -0400

    pullea mis cambios frank

--- app/Models/Dwelling/Dwelling.php
+++ app/Models/Dwelling/Dwelling.php
@@ -17,56 +17,30 @@ class Dwelling extends Model
         'city_id',
         'zone_id',
         'status',
-        'property_status',
         'property_type',
         'rooms',
         'bathrooms',
         'parking',
+        'comforts',
+        'services',
+        'images',
+        'videos',
         'details',
-        'service_details',
-        'location',
-        'currency_id',
+        'transport_details',
+        'location_details',
         'price',
+        'currency_id',
 
-        //Contact Data
-        'contactName',
-        'contactLastName',
-        'contactEmail',
-        'contactMobileNumber',
-        'contactLandlineNumber',
-        'contactLandlineNumberEXT',
-        'contactDays',
-        'contactHourFrom',
-        'contactHourTo',
+        // //Contact Data
+        // 'contactName',
+        // 'contactLastName',
+        // 'contactEmail',
+        // 'contactMobileNumber',
+        // 'contactLandlineNumber',
+        // 'contactLandlineNumberEXT',
+        // 'contactDays',
+        // 'contactHourFrom',
+        // 'contactHourTo',
 
     ];
-
-    public function remove($id){
-        $this->find($id)->delete();
-    }
-    public function edit($data){
-        
-        // $this->name_curr=$data->input("name_curr");
-
-        // $this->create();
-    }
-    
-    public function find($property){
-        return $this->where('id_curr', $property)->first();
-    }
-    
-    public function create(){
-        $this->save();
-    }
-    
-    public function isEmpty(){
-        if($this->count()==0){
-            return TRUE;
-        }else{
-            return FALSE;
-        }
-    }
-    public function findAll(){
-        return $this->all();
-    }
 }
