d6c74dcfd7904cd4779d91dfd050fba4f90f0779
fuhranku <frankponte95@gmail.com>
2020-03-19 18:06:03 -0400

    WORKING

---
+++ resources/views/dwelling_section/modify_section/part0.blade.php
@@ -0,0 +1,148 @@
+{{-- Primera parte: Dropdowns --}}
+<div class="row p-2">
+    <div class="col-md-2">
+        <div class="row"> 
+            <span class="badge badge-info">Continente</span>
+            <select id="continent-select" class="form-control sm-text">
+                <option class="disabled" value='' label="Continente" disabled value></option>
+                @foreach($continents as $continent)
+                    @if ($dwelling['continent_id'] == $continent->id)
+                        <option value={{$continent->id}} selected>{{$continent->name}}</option>
+                    @else
+                        <option value={{$continent->id}}>{{$continent->name}}</option>
+                    @endif
+                @endforeach
+            </select>
+        </div>
+        @component('components.field_error')
+            @slot('grid_size')
+                12
+            @endslot
+            @slot('error_row_id')
+                error_row_continent_select
+            @endslot
+            @slot('error_ul_id')
+                error_ul_continent_select
+            @endslot
+        @endcomponent
+    </div>
+    <div class="col-md-2">
+        <div class="row">
+            <span class="badge badge-info">Pais</span>
+            <select id="country-select" class="form-control  sm-text">
+                <option class="disabled" value='' label="País" disabled value></option>
+                @foreach($countries as $country)
+                    @if ($dwelling['country_id'] == $country->id)
+                        <option value={{$country->id}} selected>{{$country->name}}</option>
+                    @else
+                        <option value={{$country->id}}>{{$country->name}}</option>
+                    @endif
+                @endforeach
+            </select>
+        </div>
+        @component('components.field_error')
+            @slot('grid_size')
+                12
+            @endslot
+            @slot('error_row_id')
+                error_row_country_select
+            @endslot
+            @slot('error_ul_id')
+                error_ul_country_select
+            @endslot
+        @endcomponent
+    </div>
+
+    <div class="col-md-2">
+        <div class='row'>
+            <span class="badge badge-info">Estado</span>
+            <select id="state-select" class="form-control sm-text">
+                <option class="disabled" value='' label="Estado" disabled value></option>
+                @foreach($states as $state)
+                    @if ($dwelling['state_id'] == $state->id)
+                        <option value={{$state->id}} selected>{{$state->name}}</option>
+                    @else
+                        <option value={{$state->id}}>{{$state->name}}</option>
+                    @endif
+                @endforeach
+            </select>
+        </div>
+        @component('components.field_error')
+            @slot('grid_size')
+                12
+            @endslot
+            @slot('error_row_id')
+                error_row_state_select
+            @endslot
+            @slot('error_ul_id')
+                error_ul_state_select
+            @endslot
+        @endcomponent
+    </div>
+
+    <div class="col-md-2">
+        <div class="row">
+            <span class="badge badge-info">Ciudad</span>
+            <select id="city-select" class="form-control sm-text">
+                <option class="disabled" value='' label="Ciudad" disabled value></option>
+                @foreach($cities as $city)
+                @if ($dwelling['city_id'] == $city->id)
+                    <option value={{$city->id}} selected>{{$city->name}}</option>
+                @else
+                    <option value={{$city->id}}>{{$city->name}}</option>
+                @endif
+            @endforeach
+            </select>
+        </div>
+        @component('components.field_error')
+            @slot('grid_size')
+                12
+            @endslot
+            @slot('error_row_id')
+                error_row_city_select
+            @endslot
+            @slot('error_ul_id')
+                error_ul_city_select
+            @endslot
+        @endcomponent
+    </div>
+
+    <div class="col-md-2">
+        <div class="row">
+            <div class="col-md-12 ">
+                <span class="badge badge-info">Vivienda en</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 ml-3">
+                <input class="form-check-input" type="radio" value="0" name="vivienda-en-radio-btn">
+                <label class="form-check-label sm-text" >
+                    Alquiler
+                </label>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 ml-3">
+                <input class="form-check-input" type="radio" value="1" name="vivienda-en-radio-btn">
+                <label class="form-check-label sm-text">
+                    Venta
+                </label>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12 ml-3">
+                <input class="form-check-input" type="radio" value="2" name="vivienda-en-radio-btn">
+                <label class="form-check-label sm-text">
+                    Alquiler y venta
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    var countries = @json($countries);
+    var states = @json($states);
+    var cities = @json($cities);
+</script>
---
+++ resources/views/dwelling_section/modify_section/part1.blade.php
@@ -0,0 +1,174 @@
+<div class="row p-2">
+<!-- tipo de inmueble -->
+<div class="col-md-2">
+    <div class="row">
+        <div class="col-md-12 ">
+            <span class="badge badge-info">Tipo de inmueble</span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 ml-3">
+            <input class="form-check-input" type="radio" value="0" name="dwelling_type" checked>
+            <label class="form-check-label sm-text">
+                Apartamento
+            </label>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12 ml-3">
+            <input class="form-check-input" type="radio" value="1" name="dwelling_type">
+            <label class="form-check-label sm-text">
+                Casa o quinta
+            </label>
+        </div>
+    </div>
+</div>
+
+<!--  cantidad -->
+<div class="col-md-2">
+
+    <div class="row">
+        <div class="col-md-12 ">
+            <span class="badge badge-info">Cantidad</span>
+        </div>
+    </div>
+
+    <div class="row p-2">
+        <div class="col-md-4 sm-text">
+            <p >Habitaciones</p>
+        </div>
+        
+        <div class="col-md-8">
+            <div class="input-group" style="width: max-content; max-width: 120px;">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+                        <span class="fas fa-minus"></span>
+                    </button>
+                </span>
+                <input type="text" name="quant[1]" id="counterRoom" class="form-control input-number" value={{$dwelling['rooms']}} min="1" max="10">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
+                        <span class="fas fa-plus"></span>
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="row p-2">
+        <div class="col-md-4 sm-text">
+            <p>Baños</p>
+        </div>
+
+        <div class="col-md-8">
+            <div class="input-group" style="width: max-content; max-width: 120px;">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[2]">
+                        <span class="fa fa-minus"></span>
+                    </button>
+                </span>
+                <input type="text" name="quant[2]" id="counterBath" class="form-control input-number" value={{$dwelling['bathrooms']}} min="1" max="10">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[2]">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="row p-2">
+        <div class="col-md-4 sm-text">
+            <p>Estacionamientos</p>
+        </div>
+
+        <div class="col-md-8">
+            <div class="input-group" style="width: max-content; max-width: 120px;">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[3]">
+                        <span class="fa fa-minus"></span>
+                    </button>
+                </span>
+                <input type="text" name="quant[3]" id="counterPark" class="form-control input-number" value={{$dwelling['parking']}} min="1" max="10">
+                <span class="input-group-btn">
+                    <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[3]">
+                        <span class="fa fa-plus"></span>
+                    </button>
+                </span>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="col-md-2">
+    <div class="row">
+        <div class="col-md-12 form-check">
+            <ul class="checkbox-dropdown" id='checkbox-dropdown-comfort'>
+                <li class="dropdown mt-2">
+                    <p data-toggle="dropdown" class="dropdown-toggle border-gray pl-2 tiny-text">Seleccione comodidades</p>
+                    <ul class="dropdown-menu" data-flip="false">
+                        @foreach ($comforts as $comfort)
+                    <li><label class="checkbox sm-text"><input type="checkbox" value={{$comfort->id}} name="comforts">{{$comfort->name}}</label></li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    @component('components.field_error')
+        @slot('grid_size')
+            12
+        @endslot
+        @slot('error_row_id')
+            error_row_checkbox_dropdown_comfort
+        @endslot
+        @slot('error_ul_id')
+            error_ul_checkbox_dropdown_comfort
+        @endslot
+    @endcomponent
+</div>
+
+<div class="col-md-2">
+    <div class="row">
+        <div class="col-md-12 form-check">
+            <ul class="checkbox-dropdown" >
+                <li class="dropdown mt-2">
+                    <p data-toggle="dropdown" class="dropdown-toggle border-gray pl-2 sm-text">Servicios</p>
+                    <ul class="dropdown-menu" id="services-publish-dwelling-dropdown">
+                        @foreach ($services as $service)
+                            @if ($service->id != 5)
+                                <li><label class="checkbox sm-text"><input type="checkbox" value={{$service->id}} name="services">{{$service->name}}</label></li>
+                            @endif
+                        @endforeach
+                            <li><label class="checkbox sm-text"><input type="checkbox" value="other" name="services" id="service-dwelling-other-input-text">Otro</label></li>
+                            <li class='d-none' id="other-li">
+                                <div class="row sm-text" >
+                                    <div class="col-sm-12">
+                                        Especifique
+                                    </div>
+                                    <div class="col-sm-12">
+                                        <input type="text" class="form-control" id="dwelling-other-input" name='other-spec'>
+                                    </div>
+                                </div>
+                            </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    @component('components.field_error')
+    @slot('grid_size')
+        12
+    @endslot
+    @slot('error_row_id')
+        error_row_services_publish_dwelling_dropdown
+    @endslot
+    @slot('error_ul_id')
+        error_ul_services_publish_dwelling_dropdown
+    @endslot
+@endcomponent
+</div>
+</div>
