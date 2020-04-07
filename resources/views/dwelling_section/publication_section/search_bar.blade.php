4647c93613217a114d54a170c0204298bad1cb2f
fuhranku <frankponte95@gmail.com>
2020-04-06 23:39:36 -0400

    UPDATE::Code fixing

--- resources/views/dwelling_section/publication_section/search_bar.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- BARRA DE BUSQUEDA --}}
-<div class="col-md-3 search-box" style="margin-left:15px;">
-    @include('dwelling_section.publication_section.search_fast_bar')
-    @include('dwelling_section.publication_section.search_detailed_bar')
-</div>
